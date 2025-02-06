e54d73acfa208692eb57d93f28cbea719cf65c34
Irina Motoc <i.motoc@uva.nl>
2025-02-06 12:32:46 +0100

    function and test

--- example.py
+++ example.py
@@ -2,4 +2,11 @@ def add(a, b):
     return a + b
 
 def subtract(a, b):
-    return a + b  # do not change this line until prompted to do 
+    return a + b  # do not change this line until prompted to do
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+    assert add(-5, -5) == -10
+    assert add(2.5, 2.5) == 5.0 
