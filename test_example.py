3b5ae7876b812e426c043d86a0d8cc305026b577
Irina Motoc <i.motoc@uva.nl>
2025-02-06 12:43:24 +0100

    new file with test name in title

---
+++ test_example.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a + b
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+    assert add(-5, -5) == -10
+    assert add(2.5, 2.5) == 5.0
